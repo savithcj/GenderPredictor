043c64eebea85735a99e94fcbd6ece92d9a95c3f
Savith Jayasekera <scjayase@ucalgary.ca>
2019-07-15 09:43:35 -0600

    Updates

--- frontend/src/components/Info.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Info() {
-  return (
-    <div className="container-fluid my-4">
-      <p>Upload an image and get a gender prediction.</p>
-    </div>
-  );
-}
---
+++ precache-manifest.454d47e752c81ca28c5c997ba16bbb66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2802a909d8a780509cd420e7d7c9162d",
+    "url": "/GenderPredictor/index.html"
+  },
+  {
+    "revision": "e0ef523f197693174a21",
+    "url": "/GenderPredictor/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "78dfa4dcb764b136d22d",
+    "url": "/GenderPredictor/static/css/main.232305eb.chunk.css"
+  },
+  {
+    "revision": "e0ef523f197693174a21",
+    "url": "/GenderPredictor/static/js/2.073f37f5.chunk.js"
+  },
+  {
+    "revision": "78dfa4dcb764b136d22d",
+    "url": "/GenderPredictor/static/js/main.08951078.chunk.js"
+  },
+  {
+    "revision": "2ebda00b47c9d7710390",
+    "url": "/GenderPredictor/static/js/runtime~main.4175b898.js"
+  }
+]);
